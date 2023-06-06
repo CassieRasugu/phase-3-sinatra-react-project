03fd1b6b0a0322ceac9b095beb2ddb0bfe91e3cf
Cassie <cassierasugu@gmail.com>
2023-06-06 15:57:59 +0300

    Adding data sample

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
-  # Add your routes here
+  get '/cocktails' do
+  end
+  get '/drinks' do
+  end
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
 
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,40 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+# Create Cocktails
+cocktails = [
+  { name: 'Mojito' },
+  { name: 'Cosmopolitan' },
+  { name: 'Old Fashioned' },
+  { name: 'Piña Colada' },
+  { name: 'Margarita' },
+  { name: 'Negroni' }
+]
+
+cocktails.each { |cocktail| Cocktail.create(cocktail) }
+
+# Create Ingredients
+ingredients = [
+  { name: 'Lime' },
+  { name: 'Mint Leaves' },
+  { name: 'White Rum' },
+  { name: 'Simple Syrup' },
+  { name: 'Cranberry Juice' },
+  { name: 'Vodka' }
+]
+
+ingredients.each { |ingredient| Ingredient.create(ingredient) }
+
+# Create Alcoholic Drinks with Prices
+alcoholic_drinks = [
+  { name: 'Whiskey', price: 10.99 },
+  { name: 'Gin', price: 9.99 },
+  { name: 'Vodka', price: 11.99 },
+  { name: 'Tequila', price: 12.99 },
+  { name: 'Rum', price: 8.99 },
+  { name: 'Bourbon', price: 13.99 }
+]
+
+alcoholic_drinks.each { |drink| AlcoholicDrink.create(drink) }
+
 
 puts "✅ Done seeding!"
