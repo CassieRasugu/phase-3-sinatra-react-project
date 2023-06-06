64ca17489e1b9bd412586360d81d7115ad3f2ae2
Cassie <cassierasugu@gmail.com>
2023-06-06 22:27:17 +0300

    Ingredient routes

--- models/ingredient.rb
+++ models/ingredient.rb
@@ -1,7 +1,25 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :cocktail
+  has_many :cocktail_ingredients
 
   validates :name, presence: true
 end
 
-//
+  # create new ingredient
+  post '/ingredients' do
+    data = JSON.parse(request.body.read)
+    ingredient = Ingredient.create(data)
+    ingredient.to_json
+  end
+
+  # retrieve ingrdient
+  get '/ingredients' do
+    ingredients = Ingredient.all
+    ingredients.to_json
+  end
+
+  # delete ingredient
+  delete '/ingredients/:id' do
+    ingredient = Ingredient.find(params[:id])
+    ingredient.destroy
+  end
