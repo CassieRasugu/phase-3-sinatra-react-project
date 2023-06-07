d73983d7f01826568dd3c3691ae79824c9e6cdf9
Cassie <cassierasugu@gmail.com>
2023-06-07 10:41:12 +0300

    drinks migration

--- db/migrate/20230607073104_create_drinks.rb
+++ db/migrate/20230607073104_create_drinks.rb
@@ -1,4 +1,10 @@
 class CreateDrinks < ActiveRecord::Migration[6.1]
   def change
+    create_table :drinks  do |t|
+      t.string :name
+      t.string :image
+      t.integer  :price
+      t.string :description
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_07_072019) do
+ActiveRecord::Schema.define(version: 2023_06_07_073104) do
 
   create_table "cocktails", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2023_06_07_072019) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "drinks", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.integer "price"
+    t.string "description"
   end
 
 end
