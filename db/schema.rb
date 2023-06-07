57e298b83782fa79fc8d5c182a872c422f8600f6
Cassie <cassierasugu@gmail.com>
2023-06-07 10:31:56 +0300

    drinks migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_07_064910) do
+ActiveRecord::Schema.define(version: 2023_06_07_072019) do
+
+  create_table "cocktails", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
