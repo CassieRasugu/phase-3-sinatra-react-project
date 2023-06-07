06223539ee26d68c42e9c12a8d1041710ee2ea96
Cassie <cassierasugu@gmail.com>
2023-06-07 10:08:30 +0300

    creating columns

--- db/migrate/20230607064910_create_cocktails.rb
+++ db/migrate/20230607064910_create_cocktails.rb
@@ -1,4 +1,12 @@
 class CreateCocktails < ActiveRecord::Migration[6.1]
   def change
+    create_table :cocktails do |r|
+      r.string :name
+      r.string :description
+      r.string :image
+
+      r.timestamps
+    end
+    end
   end
 end
